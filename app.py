20f9a49037eaac75fe86b30e6dab4140b61ec332
Russ Shomberg <rshomberg@uri.edu>
2022-03-27 11:41:49 -0400

    light toggle working

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template, Response
-from pistreaming import PiStreamer
-from picamera import PiCamera
 from time import sleep
 from datetime import datetime
+import camera
 
 from pygtail import Pygtail
 
@@ -12,76 +11,50 @@ import logging, os
 from io import StringIO
 
 
+# Set up app
 app = Flask(__name__)
-
 app.config["SECRET_KEY"] = "SECRETKEYSECRETKEYSECRETKEYSECRETKEYSECRETKEY"
 app.config["DEBUG"] = os.environ.get("FLASK_DEBUG", False)
 app.config["JSON_AS_ASCII"] = False
 logging.basicConfig(level=logging.ERROR)
 
-
-log_dir = '/home/pi/log/'
-log_file = log_dir+datetime.now().isoformat().replace(':','')[:-7]+'.log'
-
-handler = logging.FileHandler(log_file)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-handler.setFormatter(formatter)
-
-log = logging.getLogger('camera')
-log.setLevel(logging.INFO)
-log.addHandler(handler)
-
-
-WIDTH = 640
-HEIGHT = 480
-FRAMERATE = 24
-VFLIP = False
-HFLIP = False
-
-camera = PiCamera()
-camera.resolution = 'HD'
-camera.framerate = FRAMERATE
-camera.vflip = VFLIP # flips image rightside up, as needed
-camera.hflip = HFLIP # flips image left-right, as needed
-sleep(1) # camera warm-up time
-
-streamer = PiStreamer((WIDTH,HEIGHT),camera.framerate) # TODO: impliment resize
-camera.start_recording(streamer.output,'yuv',resize=streamer.resolution)
-
-
-# logging.basicConfig(filename='/home/pi/pistreamingapp.log', level=logging.INFO,
-#                     format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s :%(message)s')
-
-
 @app.route('/')
 def index():
-    log.info("route => '/env' - hit!")
     return render_template('index.html')
 
 
 @app.route('/log')
 def show_log():
     def generate():
-        for line in Pygtail(log_file, every_n=1):
+        for line in Pygtail(camera.LOG_FILE, every_n=1):
             print(line)
             yield "data:" + str(line) + "\n\n"
             sleep(0.5)
     return Response(generate(), mimetype='text/event-stream')
 
 
-@app.route('/cmd/<cmd>')
-def cmd(cmd=None):
-    # TODO impliment camera controller
-    if cmd=='capture':
-        fname = f"/home/pi/Pictures/{datetime.now().isoformat().replace(':','')[:-7]}.jpeg"
-        camera.capture(fname, use_video_port=True)
-        log.info("Image captured")
-    else:
-        log.error("Command not recognized")
-        return f"{cmd}: failure", 200, {'Content-Type': 'text/plain'}
-    return f"{cmd}: success", 200, {'Content-Type': 'text/plain'}
+@app.route('/cmd/capture')
+def capture():
+    camera.capture()
+    return("nothing")
+
+@app.route('/cmd/startrecord')
+def start_recording():
+    camera.start_recording()
+    return("nothing")
+
+@app.route('/cmd/stoprecord')
+def stop_recording():
+    camera.stop_recording()
+    return("nothing")
+
+@app.route('/cmd/lights')
+def toggle_light():
+    camera.toggle_lights()
+    return("nothing")
 
 if __name__=='__main__':
+    camera.start_streaming()
     app.run(host='0.0.0.0', threaded=True, debug=False)
     # NOTE: changing debug to true causes problems with picamera
     
---
+++ camera.py
@@ -0,0 +1,77 @@
+import logging
+from picamera import PiCamera
+from datetime import datetime
+from time import sleep
+from pistreaming import PiStreamer
+
+import RPi.GPIO as GPIO
+
+LED_PIN = 4
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN,GPIO.OUT)
+LIGHTS_STATUS = True
+GPIO.output(LED_PIN,LIGHTS_STATUS)
+
+CAMERA_PORT = 0
+RECORDER_PORT = 1
+STREAMING_PORT = 2
+
+picam = PiCamera(camera_num=0, stereo_mode='none',
+                 stereo_decimate=False, resolution=None,
+                 framerate=None, sensor_mode=0,
+                 led_pin=None, clock_mode='reset',
+                 framerate_range=None)
+sleep(2)                        # Let camera warm up
+
+# TODO: add flip and rotate commands
+
+log_dir = '/home/pi/log/'
+LOG_FILE = log_dir+datetime.now().isoformat().replace(':','')[:-7]+'.log'
+
+handler = logging.FileHandler(LOG_FILE)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+
+log = logging.getLogger('camera')
+log.setLevel(logging.INFO)
+log.addHandler(handler)
+
+
+def now():
+    return datetime.now().isoformat().replace(':','')[:-7]    
+
+def capture():
+    output = f'/home/pi/Pictures/{now()}.jpg'
+    # NOTE: use video port to speed up capture
+    picam.capture(output, use_video_port=True, resize=None,
+                  splitter_port=CAMERA_PORT, bayer=False)
+    log.info("Image captured")
+
+def start_recording():
+    output = f'/home/pi/Videos/{now()}.h264'
+    picam.start_recording(output, resize=None,
+                          splitter_port=RECORDER_PORT)
+    log.info("Recording started")
+
+def stop_recording():
+    picam.stop_recording(splitter_port=RECORDER_PORT)
+    log.info("Recording ended")
+
+def start_streaming(resolution=(640,480)):
+    streamer = PiStreamer(resolution,picam.framerate)
+    picam.start_recording(streamer.output,'yuv',resize=streamer.resolution,
+                          splitter_port=STREAMING_PORT)
+    log.info("Websocket stream started")
+
+def stop_streaming():
+    picam.stop_recording(splitter_port=STREAMING_PORT)
+    log.info("Websocket stream ended")
+
+
+def toggle_lights(led_pin=4):
+    global LIGHTS_STATUS
+    LIGHTS_STATUS = not LIGHTS_STATUS
+    GPIO.output(LED_PIN,LIGHTS_STATUS)
+
+        
